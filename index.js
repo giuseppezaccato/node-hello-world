ea16d47e9fef8da8deb4165f940b7c20645c2da4
giuseppezaccato <giuseppezaccato@gmail.com>
2025-02-07 18:40:05 +0100

    BONUS

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
-console.log("Hello World");
+console.log("Hello Boolean");
+
+//!-------------------BONUS----------------------
+//task estraggo dalla stringa comando terminale 
+//task quello che scrivo dalla posizione DOPO al 2(compreso) con .slice(2)
+//task (che raccoglie in automatico come arguments in un array che si chiama process.argv)
+//*metodo 1 compatto con slice/join/OR|| 
+const message1 = process.argv.slice(2).join("_") || "Hello World";
+
+//*metodo 2 prima slice + join + OR||
+//task creo messaggio con condizione che se args è vuoto stampa "Hello World"
+const args2 = process.argv.slice(2);
+const message2 = args2.join('*') || "Hello World";
+
+//*metodo 3 slice + join Ternary ||
+//task stessa cosa ma con il ternary operator in maniera leggermente piu esplicita!
+const args3 = process.argv.slice(2);
+const message3 = args3 == 0 ? "Hello World" : args3.join("^");
+
+//task console spiegazione dei 3 modi con il join differente!!!
+console.log(`
+
+    metodo 1 => ${message1} 
+    metodo 2 => ${message2} 
+    metodo 3 => ${message3}
+    `
+);
